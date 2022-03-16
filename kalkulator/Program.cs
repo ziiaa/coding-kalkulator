62d7bb33f3b5de9e4dacef295754ae360a9360a6
ziiaa <azzizahnur01@students.amikom.ac.id>
2022-03-16 19:59:05 +0700

    calculator

---
+++ kalkulator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace kalkulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Aplikasi Kalkulator";
+
+            Console.Write("Inputkan Nilai a = ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Inputkan Nilai b = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.WriteLine("Hasil Penambahan {0} + {1} = {2}", a, b, penambahan(a, b));
+            Console.WriteLine("Hasil Pengurangan {0} - {1} = {2}", a, b, pengurangan(a, b));
+        }
+        {
+        static int penambahan(int a, int b)
+        { }
+        }
+
+       
+}
